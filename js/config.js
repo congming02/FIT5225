1a5a32c82eeeac7fd8a28e4f1c164e7eb56a2327
congming02 <84080029+congming02@users.noreply.github.com>
2022-02-06 13:55:46 +1100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_2Dg1dEvHJ',
+        region: 'us-east-1',
+        clientId: '4dmom0hn1uqmj6982i9mpg3r94'
+    },
+
+    api: {
+        apiGetAllUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-get-all-info", 
+        // GET
+        deleteTagUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-delete-tags", 
+        deleteImageUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-delete-image", 
+        uploadUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-upload",
+        findByTagUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-find-3-2-1",
+        fingByImageUrl: " https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-find-3-2-2",
+        delete3Url: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/delete-3"
+    }
+}
